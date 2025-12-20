020f78d5ab014b32225e8663b1e8ba46ac886bb0
GabrielBruno7 <gabriel.bruno1w@gmail.com>
2025-12-20 00:48:29 -0300

    fix: update email addresses in DatabaseSeeder for accuracy

--- app/Mail/TransferenceEmail.php
+++ app/Mail/TransferenceEmail.php
@@ -8,6 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class TransferenceEmail extends Mailable implements ShouldQueue
 {
@@ -38,6 +39,15 @@ class TransferenceEmail extends Mailable implements ShouldQueue
     public function attachments(): array
     {
         return [];
+    }
+
+    public function sendWithFailureSimulation()
+    {
+        if (random_int(0, 1) === 1) {
+            throw new \RuntimeException('Falha no envio de email');
+        }
+
+        Mail::to(env('DEFAULT_EMAIL_RECEIVER'))->send($this);
     }
 
     public function backoff(): array
